89e3585d3b38a49b8c6ba41be2eeb5f259fbab1d
Diego Ronan Kulian <dkulian@veepee.com>
2024-12-04 16:16:50 +0100

    passando lista por function

--- src/components/produtos/ListaProdutos.js
+++ src/components/produtos/ListaProdutos.js
@@ -4,16 +4,19 @@ import estilo from '../estilo';
 import produtos from './Produto';
 
 export default props => {
+  function lista() {
+    return produtos.map(p => {
+      return (
+        <Text key={p.id}>
+          {p.id} {p.nome} custa {p.preco}
+        </Text>
+      );
+    });
+  }
   return (
     <>
       <Text style={estilo.textGrande}>Lista de produtos</Text>
-      {produtos.map(p => {
-        return (
-          <Text key={p.id}>
-            {p.id} {p.nome} custa {p.preco}
-          </Text>
-        );
-      })}
+      {lista()}
     </>
   );
 };
