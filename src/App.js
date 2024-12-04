c5fe2058a7e9abcc2a6a809262170ecaf2eac07a
Diego Ronan Kulian <dkulian@veepee.com>
2024-12-04 15:51:30 +0100

    aceesando filhos dentro do componente pai

--- src/App.js
+++ src/App.js
@@ -1,35 +1,22 @@
 import React from 'react';
 import {SafeAreaView, StyleSheet} from 'react-native';
 
-import Titulo from './components/Titulo';
-import Primeiro from './components/Primeiro';
-import CompPadrao, {Comp1, Comp2} from './components/Multi';
-import Aleatorio from './components/Aleatorio';
-import MinMax from './components/MinMax';
-import Botao from './components/Botao';
-import Contador from './components/Contador';
-import Pai from './components/indireta/Pai';
-import ContadorV2 from './components/contador/ContadorV2';
-import Diferenciar from './components/Diferenciar';
-import Parimpar from './components/Parimpar';
+import Familia from './components/relacao/Familia';
+import Membro from './components/relacao/Membro';
 
 export default () => {
   return (
     <SafeAreaView style={style.App}>
-      <Parimpar num={3} />
-      {/*  <Diferenciar />
-      <ContadorV2 />
-    <Pai /> 
-    <Contador inicial={100} />
-     <Botao />
-      <Titulo titulo="Titulo principal" subtitulo="Aqui vai o subtitulo" />
-      <Aleatorio min={1} max={60} />
-      <MinMax min={213} max={20} />
-      <MinMax min={1} max={94} />
-      <CompPadrao />
-      <Comp1 />
-      <Comp2 />
-      <Primeiro /> */}
+      <Familia>
+        <Membro nome="Bia" sobrenome="Arruda" />
+        <Membro nome="Carlos" sobrenome="Arruda" />
+      </Familia>
+      <Familia>
+        <Membro nome="Julia" sobrenome="Silva" />
+        <Membro nome="Pedro" sobrenome="Silva" />
+        <Membro nome="Gui" sobrenome="Silva" />
+        <Membro nome="Fernando" sobrenome="Silva" />
+      </Familia>
     </SafeAreaView>
   );
 };
