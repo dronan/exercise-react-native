3ff885f5677ad6bdaf9d603c054351263d7aeaaa
Diego Ronan Kulian <dkulian@veepee.com>
2024-12-05 12:39:34 +0100

    trabalhando com componente de texto e botao, gerando numeros randomicos e aleatorios por definicao do input

--- src/components/mega/Mega.js
+++ src/components/mega/Mega.js
@@ -1,22 +1,39 @@
 import React, {Component} from 'react';
-import {Text, TextInput} from 'react-native';
+import {Button, Text, TextInput} from 'react-native';
 import estilo from '../estilo';
 
 export default class Mega extends Component {
   state = {
     qtdeNumeros: this.props.qtdeNumeros,
+    numeros: [],
   };
 
   alterarQtdeNumero = qtde => {
-    this.setState({qtdeNumeros: qtde});
+    this.setState({qtdeNumeros: +qtde});
+  };
+
+  gerarNumeros = () => {
+    const numeros = Array(this.state.qtdeNumeros)
+      .fill()
+      .reduce(n => {
+        const novoNumero = this.gerarNumeroNaoContido(n);
+        return [...n, novoNumero];
+      }, [])
+      .sort((a, b) => a - b);
+
+    this.setState({numeros});
+  };
+
+  gerarNumeroNaoContido = nums => {
+    const novo = parseInt(Math.random() * 60) + 1;
+    return nums.includes(novo) ? this.gerarNumeroNaoContido(nums) : novo;
   };
 
   render() {
     return (
       <>
-        <Text style={estilo.textGrande}>
-          Gerador de mega sena {this.state.qtdeNumeros}
-        </Text>
+        <Text style={estilo.textGrande}> {this.state.numeros.join(', ')} </Text>
+        <Text style={estilo.textGrande}>Gerador de mega sena</Text>
         <TextInput
           keyboardType="numeric"
           style={{borderBottomWidth: 1}}
@@ -24,6 +41,9 @@ export default class Mega extends Component {
           value={this.state.qtdeNumeros}
           onChangeText={this.alterarQtdeNumero}
         />
+        <Button title="Gerar numeros" onPress={this.gerarNumeros}>
+          Gerar numeros
+        </Button>
       </>
     );
   }
