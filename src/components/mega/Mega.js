60c0f47e58fa02081d19f52d30d8b290ca7494b6
Diego Ronan Kulian <dkulian@veepee.com>
2024-12-05 12:17:23 +0100

    uso de state em class

--- src/components/mega/Mega.js
+++ src/components/mega/Mega.js
@@ -1,13 +1,32 @@
 import React, {Component} from 'react';
-import {Text} from 'react-native';
+import {Text, TextInput} from 'react-native';
 import estilo from '../estilo';
 
 export default class Mega extends Component {
+  state = {
+    qtdeNumeros: this.props.qtdeNumeros,
+  };
+
+  constructor(props) {
+    super(props);
+  }
+
+  alterarQtdeNumero = qtde => {
+    this.setState({qtdeNumeros: qtde});
+  };
+
   render() {
     return (
-      <Text style={estilo.textGrande}>
-        Gerador de mega sena {this.props.qtdeNumeros}
-      </Text>
+      <>
+        <Text style={estilo.textGrande}>
+          Gerador de mega sena {this.state.qtdeNumeros}
+        </Text>
+        <TextInput
+          placeholder="Qtde de Números"
+          value={this.state.qtdeNumeros}
+          onChangeText={texto => this.alterarQtdeNumero(texto)}
+        />
+      </>
     );
   }
 }
